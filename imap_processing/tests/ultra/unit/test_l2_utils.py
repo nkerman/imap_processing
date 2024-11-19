22452cd3ba022d6b73dd0aa506d0fd87955e5531
nkerman <nathaniel.kerman@gmail.com>
2024-11-19 14:41:38 -0700

    rename to more general l2 utils

--- imap_processing/tests/ultra/unit/test_l2_utils.py
+++ imap_processing/tests/ultra/unit/test_l2_utils.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pytest
 
-from imap_processing.ultra.l2 import solid_angle_map
+from imap_processing.ultra.l2 import l2_utils
 
 # Parameterize with spacings (degrees here):
 valid_spacings = [0.1, 0.25, 0.5, 1, 5, 10, 20]
@@ -18,12 +18,12 @@ invalid_spacings_match_str = [
 @pytest.mark.parametrize("spacing", valid_spacings)
 def test_build_solid_angle_map(spacing):
     """Test build_solid_angle_map function."""
-    solid_angle_map_steradians = solid_angle_map.build_solid_angle_map(
+    solid_angle_map_steradians = l2_utils.build_solid_angle_map(
         spacing, input_degrees=True, output_degrees=False
     )
     assert np.isclose(np.sum(solid_angle_map_steradians), 4 * np.pi, atol=0, rtol=1e-9)
 
-    solid_angle_map_sqdeg = solid_angle_map.build_solid_angle_map(
+    solid_angle_map_sqdeg = l2_utils.build_solid_angle_map(
         np.deg2rad(spacing), input_degrees=False, output_degrees=True
     )
     assert np.isclose(
@@ -37,6 +37,6 @@ def test_build_solid_angle_map(spacing):
 def test_build_solid_angle_map_invalid_spacing(spacing, match_str):
     """Test build_solid_angle_map function raises error for invalid spacing."""
     with pytest.raises(ValueError, match=match_str):
-        _ = solid_angle_map.build_solid_angle_map(
+        _ = l2_utils.build_solid_angle_map(
             spacing, input_degrees=True, output_degrees=False
         )
