b63237ef28c0a79df03b2fb265bcb3ec58a968aa
Nat Kerman <nkerman@users.noreply.github.com>
2025-01-13 18:44:35 -0700

    Added tests

--- imap_processing/tests/ena_maps/test_map_utils.py
+++ imap_processing/tests/ena_maps/test_map_utils.py
@@ -55,6 +55,37 @@ class TestENAMapMappingUtils:
 
         np.testing.assert_equal(projection_values, expected_projection_values)
 
+    @pytest.mark.parametrize(
+        "projection_grid_shape", [(1, 1), (10, 10), (180, 360), (360, 720), (360, 180)]
+    )
+    @pytest.mark.parametrize(
+        "input_grid_shape", [(1, 1), (10, 10), (180, 360), (360, 720), (360, 180)]
+    )
+    def test_bin_single_array_at_indices_complex(
+        self, projection_grid_shape, input_grid_shape
+    ):
+        np.random.seed(0)
+        extra_axis_size = 11  # Another axis which is not spatially binned, e.g. energy
+        input_grid_size = np.prod(input_grid_shape)
+        projection_grid_size = np.prod(projection_grid_shape)
+        value_array = np.random.rand(input_grid_size, extra_axis_size)
+        input_indices = np.random.randint(0, input_grid_size, size=1000)
+        projection_indices = np.random.randint(0, projection_grid_size, size=1000)
+        projection_values = map_utils.bin_single_array_at_indices(
+            value_array,
+            input_indices=input_indices,
+            projection_indices=projection_indices,
+            projection_grid_shape=projection_grid_shape,
+        )
+
+        # Create the expected projection values by summing the input values in a loop
+        # This is different from the binning function, which uses np.bincount
+        expected_projection_values = np.zeros((projection_grid_size, extra_axis_size))
+        for ii, ip in zip(input_indices, projection_indices):
+            expected_projection_values[ip, :] += value_array[ii, :]
+
+        np.testing.assert_allclose(projection_values, expected_projection_values)
+
     # Parameterize by the size of the projection grid,
     # which is not necessarily same size as input grid
     @pytest.mark.parametrize("projection_grid_shape", [(1, 1), (10, 10), (360, 720)])
@@ -92,3 +123,60 @@ class TestENAMapMappingUtils:
         np.testing.assert_equal(
             output_dict["sum_variable_2"], expected_projection_values_2
         )
+
+    def test_bin_single_array_at_indices_3d_values_raises(self):
+        value_array = np.ones((2, 2, 2))
+        input_indices = np.array([0, 1])
+        projection_indices = np.array([0, 1])
+        projection_grid_shape = (2,)
+
+        with pytest.raises(
+            NotImplementedError,
+            match=(
+                "Only 1D and 2D arrays are supported for binning. "
+                "Received array with shape"
+            ),
+        ):
+            map_utils.bin_single_array_at_indices(
+                value_array,
+                input_indices=input_indices,
+                projection_indices=projection_indices,
+                projection_grid_shape=projection_grid_shape,
+            )
+
+    def test_bin_values_at_indices_2d_indices_raises(self):
+        input_values = {"test": np.array([1, 2, 3])}
+        input_indices = np.array([[0, 1], [1, 2]])
+        projection_indices = np.array([0, 1, 2])
+        projection_grid_shape = (3,)
+
+        with pytest.raises(
+            ValueError,
+            match=(
+                "Indices must be 1D arrays. If using a rectangular grid, "
+                "the indices must be unwrapped."
+            ),
+        ):
+            map_utils.bin_values_at_indices(
+                input_values,
+                input_indices=input_indices,
+                projection_indices=projection_indices,
+                projection_grid_shape=projection_grid_shape,
+            )
+
+    def test_bin_values_at_indices_mismatched_sizes_raises(self):
+        input_values = {"test": np.array([1, 2, 3])}
+        input_indices = np.array([0, 1, 0, 1])
+        projection_indices = np.array([0, 1, 2])
+        projection_grid_shape = (3,)
+
+        with pytest.raises(
+            ValueError,
+            match=("The number of input and projection indices must be the same"),
+        ):
+            map_utils.bin_values_at_indices(
+                input_values,
+                input_indices=input_indices,
+                projection_indices=projection_indices,
+                projection_grid_shape=projection_grid_shape,
+            )
